275b65a1984d188f624763c8e467433805995775
2021wb86560 <2021wb86560@wilp.bits-pilani.ac.in>
2025-07-14 21:05:45 +0530

    unique App.java for CI demoAdd

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("🚀 CI Pipeline Demo");
+        System.out.println("Lab4 git hub");
+        System.out.println("Build triggered at: " + LocalDateTime.now());
+    }
+}
